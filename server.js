39c9fd379d7e8ac5319c8968a4b776854a806024
Andrew Mingst <amingst97@gmail.com>
2022-06-18 12:48:06 -0400

    added axios, nuid, and web3.storage

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express')
+const axios = require('axios')
+const nuidApi = require('@nuid/sdk-nodejs').default({
+	auth: { apiKey: process.env.NUID_KEY },
+})
 
 require('dotenv').config()
 const port = process.env.PORT || 4000
