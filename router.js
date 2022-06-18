c9549ea6a0c25668aac5e8743277801f52870c0e
Andrew Mingst <amingst97@gmail.com>
2022-06-18 14:01:56 -0400

    added basic web3storage implementation

---
+++ router.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const web3store = require('./web3storage')
+
+const router = express.Router()
+
+router.post('/register', async (req, res) => {})
+
+module.exports = router
